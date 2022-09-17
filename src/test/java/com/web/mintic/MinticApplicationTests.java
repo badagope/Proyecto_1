0879db94fe1aa65c74f475a71c37066eb3b8aa8b
DongnutMac <dongnutMac@Luiss-MacBook-Pro.local>
2022-09-16 20:33:38 -0500

    ADD Java structure

---
+++ src/test/java/com/web/mintic/MinticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.mintic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
